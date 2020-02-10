8c6364896190d21c353f079aba0efed8e8ef3c0a
braulio <jonathan_b2j@hotmail.com>
2020-02-09 18:10:32 -0600

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
@@ -9,5 +8,7 @@ Rails.application.routes.draw do
   get  '/about', to:'static_pages#about'
   get  '/contact', to:'static_pages#contact'
   get '/signup', to:'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,5 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path
     assert_response :success
-  end
-  
+  end 
 end
