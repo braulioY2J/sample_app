5339071f4c7187ad4bbce1fdbc5c9ee0a4611fa0
braulioY2J <59617222+braulioY2J@users.noreply.github.com>
2020-02-14 16:21:35 -0600

    Add account activation (#7)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,20 +7,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
-    @user = User.find(params[:id])
+  # exercise 10.3.3
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
-  def index
-    @users = User.paginate(page: params[:page])
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_url && return unless @user.activated == false
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = 'Please check your email to activate your account.'
+      redirect_to root_url
+
     else
       render 'new'
     end
@@ -65,7 +68,6 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user?(@user)
   end
 
-  # Confirms an admin user.
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
